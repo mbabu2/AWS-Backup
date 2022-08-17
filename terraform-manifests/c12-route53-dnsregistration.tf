58bd41f48bd427e7d671270728207cdaeff3e1f0
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-08-17 13:02:51 -0400

    Update c12-route53-dnsregistration.tf

--- terraform-manifests/c12-route53-dnsregistration.tf
+++ terraform-manifests/c12-route53-dnsregistration.tf
@@ -6,14 +6,14 @@ resource "aws_route53_zone" "flink_aws" {
   }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.flink_aws.zone_id
-  name    = "www.flinkaws.com"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_eip.eip.public_ip]
-}
+#resource "aws_route53_record" "www" {
+# zone_id = aws_route53_zone.flink_aws.zone_id
+# name    = "www.flinkaws.com"
+# type    = "A"
+#  ttl     = "300"
+#  records = [aws_eip.eip.public_ip]
+#}
 
-output "name_server"{
-  value=aws_route53_zone.flink_aws.name_servers
-}
+#output "name_server"{
+#  value=aws_route53_zone.flink_aws.name_servers
+#}
